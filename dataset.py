fa54446a7702102a11d5872fa383a0a8fb173643
Xi-studio <110littletree@sina.com>
2023-10-09 11:50:45 +0800

    add gc

--- dataset.py
+++ dataset.py
@@ -22,13 +22,13 @@ mytransform = transforms.Compose([
 fs = gcsfs.GCSFileSystem(project='colorful-aia')
 fs.ls('era5_jy_test')
 with fs.open('era5_jy_test/radar_rain/Z_RADR_I_Z9010_20190701070601_P_DOR_SA_R_10_230_15.010_clean.png', 'rb') as f:
-    img = Image.open(io.BytesIO(f.read())
+    img = Image.open(io.BytesIO(f.read()))
     print(img)
 
 def preprocess_image(x):
-#    with fs.open('era5_jy_test/radar_rain/Z_RADR_I_Z9010_20190701070601_P_DOR_SA_R_10_230_15.010_clean.png', 'rb') as f:
-#        a = f.read()
-#        print(a)
+    name = x.replace('../data', 'ear5_jy_test')
+    with fs.open(x, 'rb') as f:
+        img = Image.open(io.BytesIO(f.read()))
     radar = Image.open(x).convert('L').resize((256, 256))
     radar = np.array(radar)
     radar = mytransform(radar)
@@ -39,7 +39,8 @@ class Radars(Dataset):
     def __init__(self,transform=None):
         super(Radars, self).__init__()
 
-        self.list = np.load('../data/2019_rain_list.npy')
+        self.list = np.load('data/2019_rain_list.npy')
+        print(self.list[0])
 
 
     def __getitem__(self, index):
@@ -58,4 +59,12 @@ class Radars(Dataset):
 
 
 if __name__ == '__main__':
-    print('ok')
+    dataset = Radars()
+    n_val = int(len(dataset) * 0.1)
+    n_train = len(dataset) - n_val
+    train_ds, val_ds = random_split(dataset, [n_train, n_val])
+    train_loader = DataLoader(train_ds, batch_size=self.batchsize, pin_memory=True, shuffle=True, num_workers=4)
+    val_loader = DataLoader(val_ds, batch_size=self.batchsize, pin_memory=True, shuffle=False, num_workers=4)
+    for x in train_loader:
+        print(x)
+        break
