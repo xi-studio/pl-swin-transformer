2afc7da4f1948b51f2cefb50ced66bf9857ecc2b
Xi-studio <110littletree@sina.com>
2023-10-09 13:51:34 +0800

    load local file

--- dataset.py
+++ dataset.py
@@ -21,17 +21,14 @@ mytransform = transforms.Compose([
         ]
     )
 
-fs = gcsfs.GCSFileSystem(project='colorful-aia')
-fs.ls('era5_jy_test')
-with fs.open('era5_jy_test/radar_rain/Z_RADR_I_Z9010_20190701070601_P_DOR_SA_R_10_230_15.010_clean.png', 'rb') as f:
-    img = Image.open(io.BytesIO(f.read()))
-    print(img)
+#fs = gcsfs.GCSFileSystem(project='colorful-aia')
+#fs.ls('era5_jy_test')
+#with fs.open('era5_jy_test/radar_rain/Z_RADR_I_Z9010_20190701070601_P_DOR_SA_R_10_230_15.010_clean.png', 'rb') as f:
+#    img = Image.open(io.BytesIO(f.read()))
+#    print(img)
 
 def preprocess_image(x):
-    name = x.replace('../data', 'ear5_jy_test')
-    with fs.open(x, 'rb') as f:
-        img = Image.open(io.BytesIO(f.read()))
-    radar = Image.open(x).convert('L').resize((256, 256))
+    radar = Image.open(x[3:]).convert('L').resize((256, 256))
     radar = np.array(radar)
     radar = mytransform(radar)
 
@@ -65,8 +62,8 @@ if __name__ == '__main__':
     n_val = int(len(dataset) * 0.1)
     n_train = len(dataset) - n_val
     train_ds, val_ds = random_split(dataset, [n_train, n_val])
-    train_loader = DataLoader(train_ds, batch_size=self.batchsize, pin_memory=True, shuffle=True, num_workers=4)
-    val_loader = DataLoader(val_ds, batch_size=self.batchsize, pin_memory=True, shuffle=False, num_workers=4)
+    train_loader = DataLoader(train_ds, batch_size=8, pin_memory=True, shuffle=True, num_workers=4)
+    val_loader = DataLoader(val_ds, batch_size=8, pin_memory=True, shuffle=False, num_workers=4)
     for x in train_loader:
-        print(x)
+        print(len(x))
         break
