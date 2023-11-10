1bc525741e93351657510854a95ca86945805522
Xi-studio <20littletree@gmail.com>
2023-11-10 11:53:31 +0800

    add era5

---
+++ era5_dataset.py
@@ -0,0 +1,64 @@
+from PIL import Image
+import torch
+import os
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader, random_split
+from torch.utils.data.distributed import DistributedSampler
+from torchvision import transforms, utils
+import glob
+import csv
+import time
+from argparse import ArgumentParser
+import gcsfs
+import io
+
+
+mytransform = transforms.Compose([
+        transforms.ToTensor(), # range [0, 255] -> [0.0,1.0]
+        ]
+    )
+
+def preprocess_image(x):
+    tp = Image.open(x).convert('L').resize((256, 256))
+    tp = np.array(tp)
+    tp = mytransform(tp)
+
+    return tp
+
+class TP(Dataset):
+    def __init__(self,transform=None):
+        super(TP, self).__init__()
+
+
+    def __getitem__(self, index):
+        tp_path = 'data/era5/tp_data/rain_%05d.png' % index
+        tp_t1   = 'data/era5/tp_data/rain_%05d.png' % (index + 1)
+        u_path  = 'data/era5/wind_data/wind_u_%05d.png' % index
+        v_path  = 'data/era5/wind_data/wind_v_%05d.png' % index
+
+        tp_img = preprocess_image(tp_path)
+        u_img  = preprocess_image(u_path)
+        v_img  = preprocess_image(v_path)
+        t1_img = preprocess_image(tp_t1)
+
+        return tp_img, u_img, v_img, t1_img 
+
+    def __len__(self):
+        return 6000
+
+
+if __name__ == '__main__':
+    dataset = TP()
+    n_val = int(len(dataset) * 0.1)
+    n_train = len(dataset) - n_val
+    train_ds, val_ds = random_split(dataset, [n_train, n_val])
+    train_loader = DataLoader(train_ds, batch_size=8, pin_memory=True, shuffle=True, num_workers=4)
+    val_loader = DataLoader(val_ds, batch_size=8, pin_memory=True, shuffle=False, num_workers=4)
+    for x in train_loader:
+        a, b, c, d = x
+        print(a.shape)
+        print(len(x))
+        break
---
+++ era5_train.py
@@ -0,0 +1,78 @@
+import os
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torchvision import transforms
+from torch.utils.data import DataLoader, random_split
+import lightning.pytorch as pl
+from lightning.pytorch.loggers import TensorBoardLogger
+import argparse
+
+from config import get_config
+from models import build_model
+from era5model import SwModel
+
+def parse_option():
+    parser = argparse.ArgumentParser('Swin Transformer training and evaluation script', add_help=False)
+    parser.add_argument('--cfg', type=str, required=True, metavar="FILE", help='path to config file', )
+    parser.add_argument(
+        "--opts",
+        help="Modify config options by adding 'KEY VALUE' pairs. ",
+        default=None,
+        nargs='+',
+    )
+
+    # easy config modification
+    parser.add_argument('--batch-size', type=int, help="batch size for single GPU")
+    parser.add_argument('--data-path', type=str, help='path to dataset')
+    parser.add_argument('--zip', action='store_true', help='use zipped dataset instead of folder dataset')
+    parser.add_argument('--cache-mode', type=str, default='part', choices=['no', 'full', 'part'],
+                        help='no: no cache, '
+                             'full: cache all data, '
+                             'part: sharding the dataset into nonoverlapping pieces and only cache one piece')
+    parser.add_argument('--pretrained',
+                        help='pretrained weight from checkpoint, could be imagenet22k pretrained weight')
+    parser.add_argument('--resume', help='resume from checkpoint')
+    parser.add_argument('--accumulation-steps', type=int, help="gradient accumulation steps")
+    parser.add_argument('--use-checkpoint', action='store_true',
+                        help="whether to use gradient checkpointing to save memory")
+    parser.add_argument('--disable_amp', action='store_true', help='Disable pytorch amp')
+    parser.add_argument('--amp-opt-level', type=str, choices=['O0', 'O1', 'O2'],
+                        help='mixed precision opt level, if O0, no amp is used (deprecated!)')
+    parser.add_argument('--output', default='output', type=str, metavar='PATH',
+                        help='root of output folder, the full path is <output>/<model_name>/<tag> (default: output)')
+    parser.add_argument('--tag', help='tag of experiment')
+    parser.add_argument('--eval', action='store_true', help='Perform evaluation only')
+    parser.add_argument('--throughput', action='store_true', help='Test throughput only')
+
+    parser.add_argument('--accelerator', type=str, help='device')
+    parser.add_argument('--devices', type=int, help='devices')
+    parser.add_argument('--max_epochs', type=int, help='max epochs')
+
+
+
+    # for acceleration
+    parser.add_argument('--fused_window_process', action='store_true',
+                        help='Fused window shift & window partition, similar for reversed part.')
+    parser.add_argument('--fused_layernorm', action='store_true', help='Use fused layernorm.')
+    ## overwrite optimizer in config (*.yaml) if specified, e.g., fused_adam/fused_lamb
+    parser.add_argument('--optim', type=str,
+                        help='overwrite optimizer if provided, can be adamw/sgd/fused_adam/fused_lamb.')
+
+    args, unparsed = parser.parse_known_args()
+
+    config = get_config(args)
+
+    return args, config
+
+
+def main(args, config):
+    model = SwModel(config)
+    #trainer = pl.Trainer(accelerator='gpu', devices=-1, max_epochs=2, enable_checkpointing=True)
+    trainer = pl.Trainer(accelerator=args.accelerator, devices=args.devices, max_epochs=args.max_epochs, enable_checkpointing=True)
+    
+    trainer.fit(model)
+
+if __name__ == "__main__":
+    args, config = parse_option()
+    main(args, config)
